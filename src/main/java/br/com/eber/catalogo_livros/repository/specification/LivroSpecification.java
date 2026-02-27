7e2de83c5968610c82613a63180d43a63ab271b8
Eber Barbosa <eberbarbosa255@gmail.com>
2026-02-27 15:09:00 -0300

    fix: reparando erro

--- src/main/java/br/com/eber/catalogo_livros/repository/specification/LivroSpecification.java
+++ src/main/java/br/com/eber/catalogo_livros/repository/specification/LivroSpecification.java
@@ -1,4 +1,22 @@
 package br.com.eber.catalogo_livros.repository.specification;
 
+import br.com.eber.catalogo_livros.model.Livro;
+import org.springframework.data.jpa.domain.Specification;
+
 public class LivroSpecification {
+
+    public static Specification<Livro> tituloContains(String titulo) {
+        return (root, query, cb) ->
+                cb.like(cb.lower(root.get("titulo")), "%" + titulo.toLowerCase() + "%");
+    }
+
+    public static Specification<Livro> autorContains(String autor) {
+        return (root, query, cb) ->
+                cb.like(cb.lower(root.get("autor")), "%" + autor.toLowerCase() + "%");
+    }
+
+    public static Specification<Livro> anoIgual(Integer ano) {
+        return (root, query, cb) ->
+                cb.equal(root.get("anoPublicacao"), ano);
+    }   
 }
