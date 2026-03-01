2d6746b330be54aaf25c221ca24cc901880156eb
Eber Barbosa <eberbarbosa255@gmail.com>
2026-03-01 17:59:52 -0300

    refactor: Refatorar LivroService para Aplicar Specifications

--- src/main/java/br/com/eber/catalogo_livros/service/LivroService.java
+++ src/main/java/br/com/eber/catalogo_livros/service/LivroService.java
@@ -3,8 +3,10 @@ package br.com.eber.catalogo_livros.service;
 import br.com.eber.catalogo_livros.exception.LivroNaoEncontradoException;
 import br.com.eber.catalogo_livros.model.Livro;
 import br.com.eber.catalogo_livros.repository.LivroRepository;
+import br.com.eber.catalogo_livros.repository.specification.LivroSpecification;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 import java.util.List;
 
@@ -24,17 +26,17 @@ public class LivroService {
     }
 
 
-   public Livro salvar(Livro livro) {
+    public Livro salvar(Livro livro) {
 
-       System.out.println("ISBN recebido: '" + livro.getIsbn() + "'");
-       System.out.println("Existe? " + livroRepository.existsByIsbn(livro.getIsbn()));
+        System.out.println("ISBN recebido: '" + livro.getIsbn() + "'");
+        System.out.println("Existe? " + livroRepository.existsByIsbn(livro.getIsbn()));
 
-       if (livroRepository.existsByIsbn(livro.getIsbn())) {
-           throw new IsbnDuplicadoException("!!! ISBN já cadastrado. !!!");
-       }
+        if (livroRepository.existsByIsbn(livro.getIsbn())) {
+            throw new IsbnDuplicadoException("!!! ISBN já cadastrado. !!!");
+        }
 
-       return livroRepository.save(livro);
-   }
+        return livroRepository.save(livro);
+    }
 
     public Livro buscarPorId(Long id) {
         return livroRepository.findById(id)
@@ -53,4 +55,25 @@ public class LivroService {
     public List<Livro> buscarPorAutor(String nomeAutor) {
         return livroRepository.findByAutorContainingIgnoreCase(nomeAutor);
     }
+
+    public List<Livro> buscarComFiltros(String titulo, String autor, Integer ano) {
+
+        Specification<Livro> spec = Specification.allOf();
+
+        if (titulo != null && !titulo.isBlank()) {
+            spec = spec.and(LivroSpecification.tituloContains(titulo));
+        }
+
+        if (autor != null && !autor.isBlank()) {
+            spec = spec.and(LivroSpecification.autorContains(autor));
+        }
+
+        if (ano != null) {
+            spec = spec.and(LivroSpecification.anoIgual(ano));
+        }
+
+        return livroRepository.findAll(spec);
+    }
+
+
 }
